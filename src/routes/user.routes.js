f89111ac62e5e9bd3830a6022527c23a5f416394
Resya-Hasan <resyahasan0@gmail.com>
2026-02-17 20:16:46 +0700

    update foto profile done

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,10 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const UserController = require('../controller/user.controllers');
-const upload = require('../middlewares/upload.middleware');
+const { uploadSingle } = require('../middlewares/uploadwrapper.middleware');
 
 router.get('/profile', UserController.getProfile);
 router.put('/profile', UserController.updateProfile);
-router.patch('/profile', upload.single('image'), UserController.updateFotoProfile);
+router.patch('/profile', (req, res, next) => {
+    console.log('middleware pertama masuk');
+    next();
+},uploadSingle('image'), UserController.updateFotoProfile);
 
 module.exports = router;
